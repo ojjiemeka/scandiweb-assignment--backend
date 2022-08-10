ba56a5b285789890a912693ecf275ddeadf0f308
ojjiemeka <63786841+ojjiemeka@users.noreply.github.com>
2022-08-10 06:25:22 +0100

    headers added

--- Api/products/index.php
+++ Api/products/index.php
@@ -1,7 +1,10 @@
 <?php
 
-header('Access-Control-Allow-Orgin: *');
-header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
+  header('Access-Control-Allow-Methods: DELETE');
+  header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
 
 include_once '../../config/Database.php';
 include_once '../../models/Products.php';
